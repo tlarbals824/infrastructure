e67200d2f8f141299904cd8a39c6f85d5850bd4e
tlarbals824 <simkm0824@naver.com>
2026-01-03 20:17:40 +0900

    Remove unsupported skip_requesting_account_id parameter

--- backend.tf
+++ backend.tf
@@ -11,7 +11,6 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
     use_path_style              = true
   }
 }
