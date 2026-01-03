741f7b635b41ca2167d4e3a0a10a9bbc6e3dea02
tlarbals824 <simkm0824@naver.com>
2026-01-03 20:19:07 +0900

    Upgrade Terraform to 1.10.3 and fix S3 backend for OCI compatibility

--- backend.tf
+++ backend.tf
@@ -11,6 +11,8 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
     use_path_style              = true
   }
 }
