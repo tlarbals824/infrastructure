a4479ab4a88b09e4228d31d088112e8793a863e1
tlarbals824 <simkm0824@naver.com>
2026-07-28 21:41:02 +0900

    style: apply terraform fmt to access.tf

--- terraform/access.tf
+++ terraform/access.tf
@@ -34,9 +34,9 @@ resource "cloudflare_zone_settings_override" "simproject_kr" {
   zone_id = var.cloudflare_zone_id
 
   settings {
-    ssl             = "strict"
-    min_tls_version = "1.2"
-    always_use_https = "on"
+    ssl                      = "strict"
+    min_tls_version          = "1.2"
+    always_use_https         = "on"
     automatic_https_rewrites = "on"
   }
 }
